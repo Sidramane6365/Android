ea1a6d3c0804d7530234ec780de237924789a2f2
icon <srramane6@gmail.com>
2020-11-09 13:16:37 +0530

    Monday

--- app/src/main/java/com/example/firebaseauth/MapFragment.java
+++ app/src/main/java/com/example/firebaseauth/MapFragment.java
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapFragment extends Fragment {
+
+public class MapFragment extends Fragment  {
 
 
     public MapFragment() {
