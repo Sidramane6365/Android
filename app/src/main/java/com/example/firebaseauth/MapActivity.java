c42d25f471d93f2866ed7864b93a0d16da956d5e
icon <srramane6@gmail.com>
2020-11-09 18:37:37 +0530

    Monday

---
+++ app/src/main/java/com/example/firebaseauth/MapActivity.java
@@ -0,0 +1,24 @@
+package com.example.firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
